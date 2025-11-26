7e8fc2a5ac0b78fcdf7bf5763bd1f4e7a70cc4d6
Riaz115 <riaz90603@gmail.com>
2025-11-26 11:32:30 +0500

    change backend url local to live

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,24 +1,24 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-// const getGraphQLUrl = () => {
-//   if (import.meta.env.VITE_GRAPHQL_URL) {
-//     return import.meta.env.VITE_GRAPHQL_URL;
-//   }
+const getGraphQLUrl = () => {
+  if (import.meta.env.VITE_GRAPHQL_URL) {
+    return import.meta.env.VITE_GRAPHQL_URL;
+  }
 
-//   if (import.meta.env.PROD) {
-//     return "/api/graphql";
-//   }
+  if (import.meta.env.PROD) {
+    return "/api/graphql";
+  }
 
-//   return "http://localhost:4000/graphql";
-// };
+  return "http://localhost:4000/graphql";
+};
 
-// const httpLink = createHttpLink({
-//   uri: getGraphQLUrl(),
-// });
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: getGraphQLUrl(),
 });
+// const httpLink = createHttpLink({
+//   uri: "http://localhost:4000/graphql",
+// });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("authToken");
